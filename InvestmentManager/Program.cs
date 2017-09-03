e5e0183860eedb02bc2e3c7a3fb300d7a9a2ddc4
Avinav <avinavksrivastav@gmail.com>
2017-09-03 12:19:48 -0400

    Add project files.

---
+++ InvestmentManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InvestmentManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
